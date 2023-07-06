3ad83e3bd1b52fd8a6b3de342c67a92df893888c
bwill210 <bwill210@fiu.edu>
2023-07-05 20:52:00 -0700

    can add book from form data, created showBooks() function

--- script.js
+++ script.js
@@ -33,48 +33,65 @@ function Book (title, author) {
     this.img_path = '';
 }
 
-function addBookToLibrary() {
-    let promptedTitle = "New Book Title";//prompt("what is the title?");
-    let promptedAuthor = "Author";//prompt("Who is the author?");
-    let book = new Book(promptedTitle, promptedAuthor);
+function addBookToLibrary(title, author) {
+    let book = new Book(title, author);
+    container.textContent = '';
     myLibrary.push(book);
-}
-
-addBookToLibrary();
-
-function openForm() {
-    document.getElementById("bookForm").style.display = "block";
-}
-
-function closeForm() {
-    document.getElementById("bookForm").style.display = "none";
-}
-
+    showBooks();
+};
 
 const container = document.querySelector(".container");
 
-for (let i = 0; i < myLibrary.length; i++) {
-    //create card element
-    const card = document.createElement("div");
-    card.className = "card";
-    const imageContainer = document.createElement("div");
-    imageContainer.className = "imgContainer";
-    //select bookObject from array
-    let bookObject = myLibrary[i];
-    //create cardImage element
-    const cardImage = document.createElement("img");
-    cardImage.src = bookObject.img_path;
-    //create section for book title
-    const bookTitle = document.createElement("h4");
-    bookTitle.textContent = bookObject.title;
-    //create p tag for book author
-    const bookAuthor = document.createElement("p");
-    bookAuthor.textContent = "by " + bookObject.author;
-    container.appendChild(card);
-    imageContainer.appendChild(cardImage);
-    card.appendChild(imageContainer);
-    card.appendChild(bookTitle);
-    card.appendChild(bookAuthor);
-}
+function showBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        //create card element
+        const card = document.createElement("div");
+        card.className = "card";
+        const imageContainer = document.createElement("div");
+        imageContainer.className = "imgContainer";
+        //select bookObject from array
+        let bookObject = myLibrary[i];
+        //create cardImage element
+        const cardImage = document.createElement("img");
+        cardImage.src = bookObject.img_path;
+        //create section for book title
+        const bookTitle = document.createElement("h4");
+        bookTitle.textContent = bookObject.title;
+        //create p tag for book author
+        const bookAuthor = document.createElement("p");
+        bookAuthor.textContent = "by " + bookObject.author;
+        container.appendChild(card);
+        imageContainer.appendChild(cardImage);
+        card.appendChild(imageContainer);
+        card.appendChild(bookTitle);
+        card.appendChild(bookAuthor);
+    };
+};
+
+//form stuff
+
+function openForm() {
+    document.getElementById("bookForm").style.display = "block";
+};
+
+function closeForm() {
+    document.getElementById("bookForm").style.display = "none";
+};
+
+const addBookForm = document.getElementById('addBook-form');
+
+addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let title = document.getElementById('title');
+    let author = document.getElementById('author');
+    if (title.value == "" || author.value == "") {
+        alert("You must enter both both a title and author");
+    }
+    addBookToLibrary(title.value, author.value);
+    title.value = '';
+    author.value = '';
+});
+
+showBooks();
 
 
