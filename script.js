2e2601f183f236d36213c3a6a90614b24473f717
bwill210 <bwill210@fiu.edu>
2023-07-06 18:54:12 -0700

    added additional attributes for book objects, modified showBooks() function and event listener on bookForm to reflect these changes. Modified card to show pages and beenRead attributes.

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ let myLibrary = [
         "title": "Harry Potter and the Sorcerer's Stone",
         "author": "J.K. Rowling",
         "img_path": "images/harryPotterAndTheSorcerersStone.jpg",
+        "pages": 462,
+        "hasBeenRead": "false",
     },
     {
         "title": "1984",
         "author": "George Orwell",
         "img_path": "images/1984.png",
+        "pages": 221,
+        "hasBeenRead": "true",
     },
     {
         "title": "Brave New World",
         "author": "Aldous Huxley",
         "img_path": "images/braveNewWorld.jpg",
+        "pages": 203,
+        "hasBeenRead": "true",
     },
     {
         "title": "Grapes of Wrath",
         "author": "John Steinbeck",
         "img_path": "images/grapesOfWrath.jpg",
+        "pages": 266,
+        "hasBeenRead": "true",
     },
 ];
 
@@ -27,14 +35,16 @@ function Book(title, author, img_path) {
     this.img_path = img_path;
 }
 
-function Book (title, author) {
+function Book (title, author, pages, hasBeenRead) {
     this.title = title;
     this.author = author;
+    this.pages = pages;
+    this.hasBeenRead = hasBeenRead;
     this.img_path = '';
 }
 
-function addBookToLibrary(title, author) {
-    let book = new Book(title, author);
+function addBookToLibrary(title, author, pages, hasBeenRead) {
+    let book = new Book(title, author, pages, hasBeenRead);
     container.textContent = '';
     myLibrary.push(book);
     showBooks();
@@ -57,14 +67,34 @@ function showBooks() {
         //create section for book title
         const bookTitle = document.createElement("h4");
         bookTitle.textContent = bookObject.title;
+        bookTitle.id = "title";
         //create p tag for book author
-        const bookAuthor = document.createElement("p");
+        const bookAuthor = document.createElement("div");
+        bookAuthor.id = "details";
         bookAuthor.textContent = "by " + bookObject.author;
+        //pages in book
+        const bookPages = document.createElement("div");
+        bookPages.id = "details";
+        bookPages.textContent = "Pages: " + bookObject.pages;
+        //has been read?
+        const beenRead = document.createElement("h4");
+        if (bookObject.hasBeenRead == "true") {
+            beenRead.textContent = "Not Read";
+            beenRead.id = "details";
+            beenRead.className = "beenRead-false"
+        }
+        else {
+            beenRead.textContent = "Read";
+            beenRead.id = "details";
+            beenRead.className = "beenRead-true"
+        }
         container.appendChild(card);
         imageContainer.appendChild(cardImage);
         card.appendChild(imageContainer);
         card.appendChild(bookTitle);
         card.appendChild(bookAuthor);
+        card.appendChild(bookPages);
+        card.appendChild(beenRead);
     };
 };
 
@@ -84,12 +114,16 @@ addBookForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let title = document.getElementById('title');
     let author = document.getElementById('author');
-    if (title.value == "" || author.value == "") {
-        alert("You must enter both both a title and author");
+    let pages = document.getElementById('pages');
+    let hasBeenRead = document.getElementById('hasBeenRead');
+    if (title.value == "" || author.value == "" || pages.value == "") {
+        alert("Must enter all fields");
     }
-    addBookToLibrary(title.value, author.value);
+    addBookToLibrary(title.value, author.value, pages.value, hasBeenRead.value);
     title.value = '';
     author.value = '';
+    pages.value = '';
+    hasBeenRead.value = '';
 });
 
 showBooks();
